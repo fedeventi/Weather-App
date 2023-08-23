d62847946907384b0dd3a35dcf281314d41f032f
fedeventi <sangreguardia@gmail.com>
2023-08-23 17:43:39 -0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Weather_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            titlebar.MouseDown += TitleBar_MouseDown;
+            City.TextChanged += TextHasChanged;
+        }
+        void TitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+        void Close(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        void Minimize(object sender, RoutedEventArgs e)
+        {
+            this.WindowState=WindowState.Minimized;
+        }
+        void TextHasChanged(object sender, TextChangedEventArgs e)
+        {
+            List<string> suggestionsList= new List<string>() {"Monte Grande", "San Francisco", "Los Angeles", "Frankfurt"  };
+
+           // suggestionMenu.IsOpen = true;
+        }
+    }
+}
