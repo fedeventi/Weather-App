88f7e04688b957af384eb845198076dbe6d16677
fedeventi <sangreguardia@gmail.com>
2023-09-01 19:21:10 -0300

    resize fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,12 +22,12 @@ namespace Weather_App
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Line> lines=new List<Line>();
        
         public MainWindow()
         {
             InitializeComponent();
             titlebar.MouseDown += TitleBar_MouseDown;
-            City.TextChanged += TextHasChanged;
             SetUpWindow();
         }
         
@@ -38,7 +38,7 @@ namespace Weather_App
         }
          void Resize(bool expand)
          {
-            
+            content.Visibility = expand?Visibility.Visible:Visibility.Collapsed;
             Storyboard showWin = (Storyboard)this.Resources["showWin"];
             Storyboard hideWin = (Storyboard)this.Resources["hideWin"];
             BeginStoryboard(expand?showWin:hideWin);
@@ -58,7 +58,7 @@ namespace Weather_App
         {
             List<string> suggestionsList= new List<string>() {"Monte Grande", "San Francisco", "Los Angeles", "Frankfurt"  };
 
-           // suggestionMenu.IsOpen = true;
+           
         }
 
         private void minimize_MouseEnter(object sender, MouseEventArgs e)
@@ -67,7 +67,8 @@ namespace Weather_App
         }
         void SetUpWindow()
         {
-            Application.Current.MainWindow.Height = 100;
+            Body.Height = 100;
+            content.Visibility = Visibility.Collapsed;
         }
 
         private void close_MouseEnter(object sender, MouseEventArgs e)
@@ -79,11 +80,12 @@ namespace Weather_App
         {
             _expand = !_expand;
             Resize(!_expand);
+            //CreateLines(4, 35);
         }
 
         private void City_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            
         }
         private void City_OnFocus(object sender, RoutedEventArgs e)
         {
@@ -94,6 +96,47 @@ namespace Weather_App
         {
             TextBox text= (TextBox)sender;
             if (text.Text.Length <= 0) text.Text = "Set your city";
+        }
+        bool isOpen;
+        private void CreateLines(int amount, int gap)
+        {
+            ClearLines(lines);
+            if(!isOpen)
+            for (int i = 0; i < amount; i++)
+            {
+                Line gradientLine = new Line();
+                gradientLine.StrokeThickness = 1;
+                gradientLine.X1 = 20;
+                gradientLine.Y1=73+gap*i;
+                gradientLine.X2 = 230;
+                gradientLine.Y2 = 73+gap*i;
+
+                LinearGradientBrush gradientBrush = new LinearGradientBrush();
+                gradientBrush.StartPoint = new Point(0, 0.5f);
+                gradientBrush.EndPoint = new Point(1, 0.5f);
+                gradientBrush.GradientStops.Add(new GradientStop(Colors.Transparent, 0));
+                gradientBrush.GradientStops.Add(new GradientStop(Colors.DarkOrange, 0.3f));
+                gradientBrush.GradientStops.Add(new GradientStop(Colors.DarkOrange, 0.7f));
+                gradientBrush.GradientStops.Add(new GradientStop(Colors.Transparent, 1));
+
+                gradientLine.Stroke = gradientBrush;
+                lines.Add(gradientLine);
+                mainCanvas.Children.Add(gradientLine);
+            }
+            isOpen = !isOpen;
+        }
+        private void ClearLines(List<Line> lines)
+        {
+            foreach (var item in lines)
+            {
+                if(mainCanvas.Children.Contains(item))
+                        mainCanvas.Children.Remove(item);
+            }
+            lines.Clear();
+        }
+        private void OpenDropdown(bool open)
+        {
+            
         }
     }
     
