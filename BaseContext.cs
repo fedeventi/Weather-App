b5db7d085b1248c1ae79f10a3849c3e1af3d69ce
fedeventi <sangreguardia@gmail.com>
2024-02-05 22:43:26 -0300

    entityframework

---
+++ BaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather_App
+{
+    internal class BaseContext : DbContext
+    {
+        public DbSet<WorldCities> WorldCities { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Server=localhost;port=3306;Database=myDatabase;User Id=root;Password=fedeventi25;database=world");
+        }
+
+    }
+}
