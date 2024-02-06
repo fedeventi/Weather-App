54fb663475a2c2fc7b6f8fe476098b6dd83c4212
fedeventi <sangreguardia@gmail.com>
2024-02-06 01:14:44 -0300

    Update BaseContext.cs

--- BaseContext.cs
+++ BaseContext.cs
@@ -4,17 +4,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Weather_App.Model;
+using Weather_App;
 
 namespace Weather_App
 {
     internal class BaseContext : DbContext
     {
-        public DbSet<WorldCities> WorldCities { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseMySQL("Server=localhost;port=3306;Database=world;User Id=root;Password=fedeventi25");
-        }
+        public BaseContext(DbContextOptions<BaseContext> options): base (options) { }
+        public DbSet<WorldCities> cities { get; set; }
+        
+       
 
     }
 }
